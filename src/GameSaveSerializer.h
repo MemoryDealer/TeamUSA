a9ee377ed1fcf21170e87d627ce154668ad71044
Vincent <jvsanchez@me.com>
2015-11-05 14:46:03 -0600

    Working on serializer module

---
+++ src/GameSaveSerializer.h
@@ -0,0 +1,33 @@
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: //
+// Team USA - Software Engineering Project (Fall 2015).
+// Legend of the Great Unwashed (Working Title).
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: //
+/// \file GameSaveSerializer.h
+/// \brief Declares save file serializer class.
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: //
+
+#pragma once
+
+//--------
+
+namespace USA {
+	
+	///
+    /// \class GameSaveSerializer
+    /// \brief Provides multithreaded save, single-thread load of save files.
+	class GameSaveSerializer {
+		private:
+			int levelID;
+			int sceneID;
+			vector<int> inventory;
+			
+		public:
+			///
+			/// \brief Loads a save file
+			void load();
+				
+			///
+			/// \brief Saves a file
+			void save()
+	}
+}
